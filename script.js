0677268cf4b1396fcc65857b54d49e05d290f05b
Shubham-0031 <sm83297@gmail.com>
2021-05-29 16:47:17 +0530

    New Links Added

--- script.js
+++ script.js
@@ -3,20 +3,9 @@ const resume  = document.getElementById('resume');
 const skills = document.getElementById('container');
 const project = document.getElementById('project1');
 const contacts = document.getElementById('contact');
+const typingSpeed = document.getElementById('check-speed');
+const bookmarks = document.getElementById('bookmarks');
+const covidCase = document.getElementById('covid-case');
+const paintClone = document.getElementById('paint');
+const deviceSpecific = document
 
-function moveUp(e){
-    if(e.target === 'About Me'){
-        console.log(e.target);
-    }
-    else if(e.target === 'Skills'){
-        console.log(e.target);
-    }
-    else if(e.target === 'Projects'){
-        console.log(e.target);
-    }
-    else if(e.target === 'Contacts'){
-        console.log(e.target);
-    }
-}
-
-document.addEventListener('click', moveUp);
